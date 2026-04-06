b2dc69410ff1eb06576c9eaf7eb52bdc9f5e6670
moseskabuswe-rgb <moses.kabuswe@gmail.com>
2026-04-06 13:18:04 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { initAnalytics } from './lib/analytics'
+import { initAnalytics, identifyUser, trackEvent } from './analytics'
 
+// 1. Initialize PostHog
 initAnalytics()
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// 2. Identify the user (any string works)
+identifyUser('test_user_1')
+
+// 3. Track a test event
+trackEvent('coffee_ordered', { coffee_type: 'latte', size: 'medium' })
