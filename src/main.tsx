bcfed9f9fb1e9e6ff4f962a8be5689dcf9059657
moseskabuswe-rgb <moses.kabuswe@gmail.com>
2026-04-06 13:29:18 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { initAnalytics, identifyUser, trackEvent } from './analytics'  // only import once
+import { initAnalytics, identifyUser, trackEvent } from './analytics'
 
 // 1. Initialize PostHog
 initAnalytics()
