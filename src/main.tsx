a1bd0ef427ec9be0e8c093cec8d08073dbe17508
moseskabuswe-rgb <moses.kabuswe@gmail.com>
2026-04-09 15:03:08 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,16 @@ import './index.css'
 import App from './App'
 import { initAnalytics } from './lib/analytics'
 
+// Unregister ALL service workers to fix CORS issues from old deployments
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (const registration of registrations) {
+      registration.unregister()
+      console.log('Unregistered SW:', registration.scope)
+    }
+  })
+}
+
 initAnalytics()
 
 createRoot(document.getElementById('root')!).render(
