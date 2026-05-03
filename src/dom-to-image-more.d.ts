5b270019790b719fc30389135974445c58091b96
moseskabuswe-rgb <moses.kabuswe@gmail.com>
2026-05-03 16:21:06 -0500

    Add files via upload

---
+++ src/dom-to-image-more.d.ts
@@ -0,0 +1,18 @@
+declare module 'dom-to-image-more' {
+  interface Options {
+    width?: number
+    height?: number
+    style?: Partial<CSSStyleDeclaration>
+    quality?: number
+    bgcolor?: string
+    imagePlaceholder?: string
+    cacheBust?: boolean
+    filter?: (node: Node) => boolean
+  }
+  function toBlob(node: Node, options?: Options): Promise<Blob>
+  function toPng(node: Node, options?: Options): Promise<string>
+  function toJpeg(node: Node, options?: Options): Promise<string>
+  function toSvg(node: Node, options?: Options): Promise<string>
+  function toCanvas(node: Node, options?: Options): Promise<HTMLCanvasElement>
+  export default { toBlob, toPng, toJpeg, toSvg, toCanvas }
+}
